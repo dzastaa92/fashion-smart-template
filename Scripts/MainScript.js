58394e865e73f93adea6fb121d78de93d13fbb82
Justyna <justyna.wojciechowska04@gmail.com>
2016-04-21 12:28:34 +0200

    updated footer

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -79,3 +79,9 @@ $(document).ready(function () {
 
 })
 ;
+
+$(function () {
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
+        $('.navbar-toggle:visible').click();
+    });
+});
