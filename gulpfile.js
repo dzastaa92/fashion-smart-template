32a2a921bcd513a8978c6ba56b721f59e5b2062f
Justyna <justyna.wojciechowska04@gmail.com>
2016-05-17 12:27:58 +0200

    minify css styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var css_minify = require('gulp-minify-css');
 
 gulp.task('sass', function () {
     return gulp.src('sass_files/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('pre-css'));
 });
 
 gulp.task('watch', function () {
@@ -16,4 +17,10 @@ gulp.task('watch', function () {
 gulp.task('default', function() {
     gulp.start('sass');
     gulp.start('watch');
+});
+
+gulp.task('minify-css', function () {
+   return gulp.src('pre-css/AllStyles.css')
+       .pipe(css_minify({keepSpecialComments: 1}))
+       .pipe(gulp.dest('css'))
 });
